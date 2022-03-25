c2e0a71efaa4c178bb6ccc764f6abb5bc72ab07d
andrewettensohn <andrewettensohn@gmail.com>
2022-03-25 13:13:26 -0400

    undo test changes

--- ProbablyFriends/Program.cs
+++ ProbablyFriends/Program.cs
@@ -1,43 +1,26 @@
-using Microsoft.Extensions.Hosting.WindowsServices;
-using System.Diagnostics;
+var builder = WebApplication.CreateBuilder(args);
 
-try
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    var options = new WebApplicationOptions
-    {
-        Args = args,
-        ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default
-    };
-
-    var builder = WebApplication.CreateBuilder(options);
-
-    builder.Host.UseWindowsService();
-
-    // Add services to the container.
-    builder.Services.AddRazorPages();
-
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (!app.Environment.IsDevelopment())
-    {
-        app.UseExceptionHandler("/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
-
-    app.UseHttpsRedirection();
-    app.UseStaticFiles();
-
-    app.UseRouting();
-
-    app.UseAuthorization();
-
-    app.MapRazorPages();
-
-    app.Run();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
-catch (Exception ex)
-{
-    EventLog.WriteEntry("Application", ex.ToString(), EventLogEntryType.Error);
-}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
