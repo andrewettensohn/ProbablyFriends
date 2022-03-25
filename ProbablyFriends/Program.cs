be4030940550adb1915d4b57f9070c1c8c2e2b81
andrewettensohn <andrewettensohn@gmail.com>
2022-03-25 13:00:14 -0400

    Add event logging

--- ProbablyFriends/Program.cs
+++ ProbablyFriends/Program.cs
@@ -1,35 +1,43 @@
 using Microsoft.Extensions.Hosting.WindowsServices;
+using System.Diagnostics;
 
-var options = new WebApplicationOptions
+try
 {
-    Args = args,
-    ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default
-};
+    var options = new WebApplicationOptions
+    {
+        Args = args,
+        ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default
+    };
 
-var builder = WebApplication.CreateBuilder(options);
+    var builder = WebApplication.CreateBuilder(options);
 
-builder.Host.UseWindowsService();
+    builder.Host.UseWindowsService();
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+    // Add services to the container.
+    builder.Services.AddRazorPages();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapRazorPages();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
+catch (Exception ex)
+{
+    EventLog.WriteEntry("Application", ex.ToString(), EventLogEntryType.Error);
+}
